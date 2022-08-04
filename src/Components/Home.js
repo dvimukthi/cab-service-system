642da0251628e3434e3f7f8ee38835cf018c5059
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-04 18:05:08 +0530

    Add BodyPartThree

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BodyPartOne from "./HomeComponents/BodyPartOne/BodyPartOne";
+import BodyPartThree from "./HomeComponents/BodyPartThree/BodyPartThree";
 import BodyPartTwo from "./HomeComponents/BodyPartTwo/BodyPartTwo";
 import Header from "./HomeComponents/Header/Header";
 import Navbar from "./HomeComponents/Navbar/Navbar";
@@ -11,6 +12,7 @@ function Home() {
       <Header />
       <BodyPartOne />
       <BodyPartTwo />
+      <BodyPartThree />
     </div>
   );
 }
---
+++ src/Components/HomeComponents/BodyPartThree/BodyPartThree.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../BodyPartThree/BodyPartThree.css";
+
+function BodyPartThree() {
+  return (
+    <div className="bodypartthree__container">
+      <h2>How GoCheetah Works</h2>
+    </div>
+  );
+}
+
+export default BodyPartThree;
