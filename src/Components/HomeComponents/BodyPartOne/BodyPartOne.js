d598d34baaf8c4d48495bf89e2e50fa158b6d215
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-05 14:07:32 +0530

    BodyPartOne img

--- src/Components/HomeComponents/BodyPartOne/BodyPartOne.js
+++ src/Components/HomeComponents/BodyPartOne/BodyPartOne.js
@@ -1,11 +1,39 @@
 import React from "react";
 import "../BodyPartOne/BodyPartOne.css";
 import RIDEIMG from "../../../Assets/need a ride.jpg";
+import PROMOTIONIMG from "../../../Assets/GoCheeta poster.png";
 
 function BodyPartOne() {
   return (
     <div className="bodypartone__container">
-      <div className="bodypartone__part1"></div>
+      <div className="bodypartone__part1">
+        <div className="box">
+          <figure>
+            <img
+              src={PROMOTIONIMG}
+              alt=""
+              width="729.2"
+              height="560"
+              style={{
+                borderRadius: "10px",
+              }}
+            />
+            <figcaption>
+              <h1>GoCheeta Taxi Service</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+
       <div className="bodypartone__part2">
         <img
           src={RIDEIMG}
--- src/Components/HomeComponents/Navbar/Navbar.js
+++ src/Components/HomeComponents/Navbar/Navbar.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "../Navbar/Navbar.css";
-import LOGO from "../../../Assets/applogo.png";
+import LOGO from "../../../Assets/GoCheeta logo.png";
 
 function Navbar() {
   return (
     <div className="container">
       <div className="logo__img">
-        <img src={LOGO} alt="" />
+        <img
+          src={LOGO}
+          alt=""
+          width="70"
+          height="70"
+          style={{
+            marginLeft: "7px",
+          }}
+        />
       </div>
       <div className="text__container">
         <div className="nav__text1">Sign in</div>
