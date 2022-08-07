b3e234f40a387e0ff406e00a7fd13b2b83052281
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-07 17:12:46 +0530

    booking placeholder

--- src/Components/BookingComponent/BookingBody/BookingBody.js
+++ src/Components/BookingComponent/BookingBody/BookingBody.js
@@ -4,12 +4,25 @@ import "../BookingBody/BookingBody.css";
 function BookingBody() {
   return (
     <div className="bookingBody__container">
+      <h1>Plan Your Trip</h1>
       <form>
-        <h1>Plan Your Trip</h1>
         <div className="Trip__container">
           <div className="BookingField">
             <label>Pickup Location</label>
-            <input type="text" name="Pickup Location" />
+            <input
+              type="text"
+              name="PickupLocation"
+              placeholder="Pickup Location"
+            />
+          </div>
+          <div className="BookingField">
+            <label>Drop Location</label>
+            <input
+              type="text"
+              name="StreetAddress"
+              placeholder="Street Address"
+            />
+            <input type="text" name="city" placeholder="City" />
           </div>
         </div>
       </form>
