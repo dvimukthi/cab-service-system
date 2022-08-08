0502eb8f3f8e2d718c38d8e72d7815f8c27cbc1a
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-08 20:31:34 +0530

    add signup img

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SignUpBody from "./SignUpComponents/SignUpBody";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <SignUpBody />
+    </div>
+  );
 }
 
 export default SignUp;
