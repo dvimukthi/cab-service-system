9e837819416376257c79c973b1f39acc1be0d853
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-08 14:10:00 +0530

    Checkbox

--- src/Components/BookingComponent/BookingBody/BookingVehicles.js
+++ src/Components/BookingComponent/BookingBody/BookingVehicles.js
@@ -1,8 +1,42 @@
 import React from "react";
 import "./BookingVehicles.css";
+import { orange } from "@mui/material/colors";
+import Checkbox from "@mui/material/Checkbox";
+
+const label = { inputProps: { "aria-label": "Checkbox bookingVehicles" } };
 
 function BookingVehicles() {
-  return <div>BookingVehicles</div>;
+  return (
+    <div>
+      <Checkbox
+        {...label}
+        sx={{
+          color: orange[800],
+          "&.Mui-checked": {
+            color: orange[600],
+          },
+        }}
+      />
+      <Checkbox
+        {...label}
+        sx={{
+          color: orange[800],
+          "&.Mui-checked": {
+            color: orange[600],
+          },
+        }}
+      />
+      <Checkbox
+        {...label}
+        sx={{
+          color: orange[800],
+          "&.Mui-checked": {
+            color: orange[600],
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default BookingVehicles;
