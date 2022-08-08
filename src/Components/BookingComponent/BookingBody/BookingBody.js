1ca64ddfdf574f29e543f71b08f8023b9ef17b26
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-08 16:38:37 +0530

    add BOOKINGPAGEIMG

--- src/Components/BookingComponent/BookingBody/BookingBody.js
+++ src/Components/BookingComponent/BookingBody/BookingBody.js
@@ -3,58 +3,74 @@ import "../BookingBody/BookingBody.css";
 import BookingVehicles from "./BookingVehicles";
 import DropdownBranches from "./DropdownBranches";
 
+import BOOKINGPAGEIMG from "../../../Assets/BookingImg/bookingpage.png";
+
 function BookingBody() {
   const [selected, setSelected] = useState("");
 
   return (
     <div className="bookingBody__container">
-      <h1>Plan Your Trip</h1>
-      <form>
-        <div className="Trip__container">
-          <div className="BookingField">
-            <label>Pickup Location</label>
-            <input
-              type="text"
-              name="PickupLocation"
-              placeholder="Pickup Location"
-              style={{
-                marginTop: "10px",
-                marginRight: "85%",
-                height: "25px",
-                borderRadius: "4px",
-              }}
-            />
+      <div className="boookingparttwo__container">
+        <form>
+          <h1>Plan Your Trip</h1>
+          <div className="Trip__container">
+            <div className="BookingField">
+              <label>Pickup Location</label>
+              <input
+                type="text"
+                name="PickupLocation"
+                placeholder="Pickup Location"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "85%",
+                  height: "25px",
+                  borderRadius: "4px",
+                }}
+              />
+            </div>
+            <label>Select your nearest branch</label>
+            <DropdownBranches selected={selected} setSelected={setSelected} />
+            <div className="BookingField">
+              <label>Drop Location</label>
+              <input
+                type="text"
+                name="StreetAddress"
+                placeholder="Street Address"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "85%",
+                  height: "25px",
+                  borderRadius: "4px",
+                }}
+              />
+              <input
+                type="text"
+                name="city"
+                placeholder="City"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "85%",
+                  height: "25px",
+                  borderRadius: "4px",
+                }}
+              />
+            </div>
           </div>
-          <label>Select your nearest branch</label>
-          <DropdownBranches selected={selected} setSelected={setSelected} />
-          <div className="BookingField">
-            <label>Drop Location</label>
-            <input
-              type="text"
-              name="StreetAddress"
-              placeholder="Street Address"
-              style={{
-                marginTop: "10px",
-                marginRight: "85%",
-                height: "25px",
-                borderRadius: "4px",
-              }}
-            />
-            <input
-              type="text"
-              name="city"
-              placeholder="City"
-              style={{
-                marginTop: "10px",
-                marginRight: "85%",
-                height: "25px",
-                borderRadius: "4px",
-              }}
-            />
-          </div>
-        </div>
-      </form>
-      <BookingVehicles />
+        </form>
+        <BookingVehicles />
+      </div>
+      <img
+        src={BOOKINGPAGEIMG}
+        alt=""
+        width="600"
+        height="600"
+        style={{
+          marginTop: "20px",
+          marginBottom: "5px",
+          marginRight: "70px",
+          borderRadius: "300px",
+        }}
+      />
     </div>
   );
 }
