a366564399c64bea89358ea13a6fc007ec368013
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-08 12:27:30 +0530

    dropdown useState

--- src/Components/BookingComponent/BookingBody/DropdownBranches.js
+++ src/Components/BookingComponent/BookingBody/DropdownBranches.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DropdownBranches.css";
 import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
 
 function DropdownBranches() {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <div className="dropdown__container">
-      <div className="dopdown__btn">
+      <div className="dropdown__btn">
         Branches
         <ArrowDropDownRoundedIcon />
       </div>
-      <div className="dopdown__content">
-        <div className="dopdown__item">Galle</div>
-        <div className="dopdown__item">Kandy</div>
-        <div className="dopdown__item">Nugegoda</div>
-        <div className="dopdown__item">Gampaha</div>
-        <div className="dopdown__item">Kurunegala</div>
-        <div className="dopdown__item">Jaffna</div>
+      <div className="dropdown__content">
+        <div className="dropdown__item">Galle</div>
+        <div className="dropdown__item">Kandy</div>
+        <div className="dropdown__item">Nugegoda</div>
+        <div className="dropdown__item">Gampaha</div>
+        <div className="dropdown__item">Kurunegala</div>
+        <div className="dropdown__item">Jaffna</div>
       </div>
     </div>
   );
