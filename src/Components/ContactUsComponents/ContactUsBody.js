c252afd385f6a34b03706945682bd54aca0af41b
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-09 14:54:31 +0530

    ContactUs style

--- src/Components/ContactUsComponents/ContactUsBody.js
+++ src/Components/ContactUsComponents/ContactUsBody.js
@@ -32,7 +32,45 @@ function ContactUsBody() {
         </div>
       </div>
       <div className="ContactUs__partTwo">
-        <h2>Branches</h2>
+        <div className="PartTwo__text">
+          <h2>Branches</h2>
+          <p>
+            <h3>Galle Branch</h3>
+            +94 0666666666
+            <br />
+            galle@gocheeta.com
+          </p>
+          <p>
+            <h3>Kandy Branch</h3>
+            +94 0777777777
+            <br />
+            Kandy@gocheeta.com
+          </p>
+          <p>
+            <h3>Nugegoda Branch</h3>
+            +94 0888888888
+            <br />
+            Nugegoda@gocheeta.com
+          </p>
+          <p>
+            <h3>Gampaha Branch</h3>
+            +94 0999999999
+            <br />
+            Gampaha@gocheeta.com
+          </p>
+          <p>
+            <h3>Kurunegala Branch</h3>
+            +94 0011111111
+            <br />
+            Kurunegala@gocheeta.com
+          </p>
+          <p>
+            <h3>jaffna Branch</h3>
+            +94 0022222222
+            <br />
+            jaffna@gocheeta.com
+          </p>
+        </div>
       </div>
     </div>
   );
