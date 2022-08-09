7760c72808248eb9cafeaf4f1b0c446b55a290a3
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-09 15:13:21 +0530

    AboutUs__partOne

--- src/Components/AboutUsComponents/AboutUsBody/AboutUsBody.js
+++ src/Components/AboutUsComponents/AboutUsBody/AboutUsBody.js
@@ -2,7 +2,27 @@ import React from "react";
 import "../AboutUsBody/AboutUsBody.css";
 
 function AboutUsBody() {
-  return <div>AboutUsBody</div>;
+  return (
+    <div className="AboutUs__Container">
+      <div className="AboutUs__partOne">
+        <div className="PartOne">
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+      <div className="AboutUs__partTwo">
+        <div className="PartTwo"></div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUsBody;
