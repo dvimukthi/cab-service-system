16e81dcb73e1506a0641a3dfca7100cc03a7480b
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-11 00:26:14 +0530

    nextSlide & prevSlide Array

--- src/Components/HomeComponents/Slideshow/Slideshow.js
+++ src/Components/HomeComponents/Slideshow/Slideshow.js
@@ -5,21 +5,32 @@ import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRound
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 
 const Slideshow = ({ slides }) => {
-  const { taxi, setTaxi } = useState(0);
+  const [current, setCurrent] = useState(0);
   const length = slides.length;
 
   const nextSlide = () => {
-    setTaxi(taxi === length - 1 ? 0 : taxi + 1);
+    setCurrent(current === length - 1 ? 0 : current + 1);
   };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  console.log(current);
 
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
-
   return (
     <section className="slider">
-      <ArrowForwardIosRoundedIcon className="RightArrowIcon" />
-      <ArrowBackIosNewRoundedIcon className="leftArrowIcon" />
+      <ArrowForwardIosRoundedIcon
+        className="leftArrowIcon"
+        onClick={nextSlide}
+      />
+      <ArrowBackIosNewRoundedIcon
+        className="RightArrowIcon"
+        onClick={prevSlide}
+      />
       {SlideshowData.map((slide, index) => {
         return <img src={slide.Image} alt="taxi image" className="Image" />;
       })}
