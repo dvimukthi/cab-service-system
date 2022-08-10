679a9829093a178502a6b2eb2ac5336a0342505e
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-10 23:31:24 +0530

    import arrow icons

--- src/Components/HomeComponents/Slideshow/Slideshow.js
+++ src/Components/HomeComponents/Slideshow/Slideshow.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { SlideshowData } from "./SlideshowData";
+import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 
 const Slideshow = () => {
+  const { taxi, setTaxi } = useState(0);
+  const length = slides.length;
+
   return (
     <>
       {SlideshowData.map((slide, index) => {
