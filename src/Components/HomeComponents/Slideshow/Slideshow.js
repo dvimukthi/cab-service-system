a87fa7c4bfc3d358212c750685965896978e430c
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-11 00:10:48 +0530

    nextSlide

--- src/Components/HomeComponents/Slideshow/Slideshow.js
+++ src/Components/HomeComponents/Slideshow/Slideshow.js
@@ -8,10 +8,18 @@ const Slideshow = ({ slides }) => {
   const { taxi, setTaxi } = useState(0);
   const length = slides.length;
 
+  const nextSlide = () => {
+    setTaxi(taxi === length - 1 ? 0 : taxi + 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
     <section className="slider">
-      <ArrowBackIosNewRoundedIcon className="leftArrowIcon" />
       <ArrowForwardIosRoundedIcon className="RightArrowIcon" />
+      <ArrowBackIosNewRoundedIcon className="leftArrowIcon" />
       {SlideshowData.map((slide, index) => {
         return <img src={slide.Image} alt="taxi image" className="Image" />;
       })}
