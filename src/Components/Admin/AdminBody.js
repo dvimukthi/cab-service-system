556cc1fa833dd3211a3d61d99be88e1cf55cd3e1
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-10 21:38:11 +0530

    AdminMain

--- src/Components/Admin/AdminBody.js
+++ src/Components/Admin/AdminBody.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./AdminBody.css";
+import AdminMain from "./AdminMain";
 import AdminSidebar from "./AdminSidebar";
 
 function AdminBody() {
   return (
     <div>
       <AdminSidebar />
+      <AdminMain />
     </div>
   );
 }
