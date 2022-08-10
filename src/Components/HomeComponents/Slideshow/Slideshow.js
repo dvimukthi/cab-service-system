d680ee07bf39d3cff6cbcb4bc733de5442b9faef
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-11 00:02:47 +0530

    Slideshow styles

--- src/Components/HomeComponents/Slideshow/Slideshow.js
+++ src/Components/HomeComponents/Slideshow/Slideshow.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Slideshow.css";
 import { SlideshowData } from "./SlideshowData";
 import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
@@ -8,13 +9,13 @@ const Slideshow = ({ slides }) => {
   const length = slides.length;
 
   return (
-    <div className="slider">
+    <section className="slider">
       <ArrowBackIosNewRoundedIcon className="leftArrowIcon" />
       <ArrowForwardIosRoundedIcon className="RightArrowIcon" />
       {SlideshowData.map((slide, index) => {
-        return <img src={slide.Image} alt="taxi image" />;
+        return <img src={slide.Image} alt="taxi image" className="Image" />;
       })}
-    </div>
+    </section>
   );
 };
 
