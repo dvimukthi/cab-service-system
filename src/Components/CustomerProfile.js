c0c54c22deea9536aa5d93e2af61258e5b805e20
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-11 23:44:52 +0530

    add CustomerBody

--- src/Components/CustomerProfile.js
+++ src/Components/CustomerProfile.js
@@ -1,10 +1,12 @@
 import React from "react";
+import CustomerBody from "./CustomerProfile/CustomerBody";
 import CustomerHeader from "./CustomerProfile/CustomerHeader";
 
 function CustomerProfile() {
   return (
     <div>
       <CustomerHeader />
+      <CustomerBody />
     </div>
   );
 }
