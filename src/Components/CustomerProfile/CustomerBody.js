63821dfdd393b7c528d51fa2eebf67bf59cf087d
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-12 00:45:38 +0530

    Details styles

--- src/Components/CustomerProfile/CustomerBody.js
+++ src/Components/CustomerProfile/CustomerBody.js
@@ -1,8 +1,36 @@
 import React from "react";
 import "./CustomerBody.css";
+import UserProfile from "../../Assets/user-profile-img.png";
 
 function CustomerBody() {
-  return <div>CustomerBody</div>;
+  return (
+    <div className="CustomerBody__Container">
+      {/* Add Customer Details */}
+      <div className="ProfileDetails__Container">
+        <h2>Your Profile</h2>
+        <div className="Details__Container">
+          {/* customer image part */}
+          <img
+            src={UserProfile}
+            alt=""
+            width="90"
+            height="90"
+            style={{
+              marginRight: "10px",
+              borderRadius: "10px",
+              border: "3px solid  black",
+              background: "white",
+            }}
+          />
+          {/* customer details part */}
+          <div className="Details">
+            <h3>Name: </h3>
+            <h3>Email: </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CustomerBody;
