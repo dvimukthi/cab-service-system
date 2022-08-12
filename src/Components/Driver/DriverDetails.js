f3d4b374abf95726f1a7da0cf96b3281d5e22fd8
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-13 00:54:13 +0530

    driver details

--- src/Components/Driver/DriverDetails.js
+++ src/Components/Driver/DriverDetails.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./DriverDetails.css";
 
 function DriverDetails() {
-  return <div>DriverDetails</div>;
+  return (
+    <div className="DriverProfile__Container">
+      {/* driver details */}
+      <h1>Driver Profile</h1>
+      <p>
+        <h2>Driver Name: </h2>
+        <h2>Driver Email: </h2>
+        <h2>Branch: </h2>
+        <h2>Vehicle Type: </h2>
+        <h2>Vehicle Name: </h2>
+        <h2>Vehicle Number: </h2>
+      </p>
+    </div>
+  );
 }
 
 export default DriverDetails;
