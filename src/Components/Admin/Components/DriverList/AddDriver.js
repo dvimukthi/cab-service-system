b6181d7ea51ff3e092db023697c68d9246dc1d4d
dvimukthi <deshanvimukthi59@gmail.com>
2022-09-17 14:37:49 +0530

    Driver register form

---
+++ src/Components/Admin/Components/DriverList/AddDriver.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import "./AddDriver.css";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+import DriverVehicleType from "./DriverVehicleType";
+import DriverBranch from "./DriverBranch";
+
+function AddDriver() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add Driver
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle className="DriverRegister__container">
+          Add a Driver
+        </DialogTitle>
+
+        <form>
+          <div className="Register__Form">
+            <div className="DriverRegisterField">
+              <label>Name</label>
+              <input
+                type="text"
+                name="DriverName"
+                placeholder="Enter Driver Name"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "38%",
+                  height: "28px",
+                  borderRadius: "4px",
+                }}
+              />
+
+              <label>Email</label>
+              <input
+                type="text"
+                name="DriverEmail"
+                placeholder="Enter Driver Email"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "38%",
+                  height: "28px",
+                  borderRadius: "4px",
+                }}
+              />
+
+              <label>Phone Number</label>
+              <input
+                type="text"
+                name="DriverPhoneNumber"
+                placeholder="Enter Driver Phone Number"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "38%",
+                  height: "28px",
+                  borderRadius: "4px",
+                }}
+              />
+              <label>Password</label>
+              <input
+                type="text"
+                name="DriverPassword"
+                placeholder="Enter Password"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "38%",
+                  height: "28px",
+                  borderRadius: "4px",
+                }}
+              />
+
+              <label>Select Driver's Vehicle Type</label>
+              {/* dropdown vehicle type box */}
+              <DriverVehicleType
+                selected={selected}
+                setSelected={setSelected}
+              />
+
+              <label>Vehicle Name</label>
+              <input
+                type="text"
+                name="DriverName"
+                placeholder="Enter Vehicle Name"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "38%",
+                  height: "28px",
+                  borderRadius: "4px",
+                }}
+              />
+
+              <label>Vehicle NumberPlate</label>
+              <input
+                type="text"
+                name="DriverNumberPlate"
+                placeholder="Enter Vehicle NumberPlate"
+                style={{
+                  marginTop: "10px",
+                  marginRight: "38%",
+                  height: "28px",
+                  borderRadius: "4px",
+                }}
+              />
+              <label>Select Driver's Branch</label>
+              {/* dropdown driver branch box */}
+              <DriverBranch selected={selected} setSelected={setSelected} />
+            </div>
+
+            <div className="DriverBtn">Add Driver</div>
+          </div>
+        </form>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
+
+export default AddDriver;
