0b8f8a6d68c850bc3f25033aae5de5b94d050c03
dvimukthi <deshanvimukthi59@gmail.com>
2022-09-17 12:54:38 +0530

    admin components

--- src/Components/Admin/AdminBody.js
+++ src/Components/Admin/AdminBody.js
@@ -3,11 +3,36 @@ import "./AdminBody.css";
 import AdminMain from "./AdminMain";
 import AdminSidebar from "./AdminSidebar";
 
-function AdminBody() {
+import Box from "@mui/material/Box";
+
+import DriverList from "./Components/DriverList";
+import AddVehicles from "./Components/AddVehicles";
+import AddVehicleCategories from "./Components/AddVehicleCategories";
+import Branch from "./Components/Branch";
+
+function AdminBody(props) {
+  const { window, widget } = props;
+
   return (
     <div className="AdminBody__Container">
       <AdminSidebar />
       <AdminMain />
+
+      <Box>
+        {widget === "driverlist" ? (
+          <DriverList />
+        ) : widget === "addvehicles" ? (
+          <AddVehicles />
+        ) : widget === "addvehiclecategories" ? (
+          <AddVehicleCategories />
+        ) : widget === "branches" ? (
+          <Branch />
+        ) : (
+          {
+            /* <Tabs /> */
+          }
+        )}
+      </Box>
     </div>
   );
 }
