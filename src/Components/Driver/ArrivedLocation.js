dec54622c3564e33fa74428a84f395e834899fd7
dvimukthi <deshanvimukthi59@gmail.com>
2022-09-18 13:53:13 +0530

    ArrivedLocation

--- src/Components/Driver/ArrivedLocation.js
+++ src/Components/Driver/ArrivedLocation.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./ArrivedLocation.css";
+import ArrivedLocationIMG from "../../Assets/locationimage.png";
+import { useNavigate } from "react-router";
 
 function ArrivedLocation() {
+  const navigate = useNavigate();
+
+  const loadDriver = () => {
+    navigate("/driver");
+  };
+
   return (
     <div className="ArrivedLocation__Container">
-      <h2>You arrived at the location</h2>
+      <div className="ArrivedLocation__Img">
+        <img
+          src={ArrivedLocationIMG}
+          alt=""
+          width="450"
+          height="450"
+          style={{
+            marginTop: "40px",
+            borderRadius: "10px",
+          }}
+        />
+      </div>
+      <h2>You Arrived To The Location</h2>
+      <div className="ArrivedLocation__btn" onClick={loadDriver}>
+        End Trip
+      </div>
     </div>
   );
 }
